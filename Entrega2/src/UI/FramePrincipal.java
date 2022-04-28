9e5f169ac06d778f4d3646f351e8e59a35044c93
Pau3 <p.dazad@uniandes.edu.co>
2022-04-27 19:13:16 -0500

    Adelanto Panel Calendario y TipoActividad

--- Entrega2/src/UI/FramePrincipal.java
+++ Entrega2/src/UI/FramePrincipal.java
@@ -1,13 +1,18 @@
 package UI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class FramePrincipal extends JFrame {
 	
 	//Atributos
-	
+	JLabel lblTitulo;
+	PTipoActividad pTipoActividad;
 	
 	//Constructor
 	public FramePrincipal() {
@@ -17,12 +22,25 @@ public class FramePrincipal extends JFrame {
 		setLocationRelativeTo(null);
 		setTitle("Project Manager");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		PBotones botones = new PBotones();
-		add(botones, BorderLayout.WEST);
+		iniciarComponentes();
 		
 	}
 	
+	private void iniciarComponentes() {
+		pTipoActividad = new PTipoActividad();
+		pTipoActividad.setBackground(Color.cyan);//Solo pa ver si funciona jsjjs, el color es horrible.
+		PBotones botones = new PBotones();
+		lblTitulo = new JLabel ("¿Qué harás hoy en tu proyecto?");
+		lblTitulo.setFont(new Font("Open Sans ExtraBold", Font.BOLD, 30));
+		
+		add(botones, BorderLayout.WEST);
+		add(pTipoActividad, BorderLayout.CENTER);
+		add(lblTitulo,BorderLayout.NORTH);
+		
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void main(String[] args) {
 		FramePrincipal framePrincipal = new FramePrincipal();
 		framePrincipal.setVisible(true);
--- Entrega2/src/UI/PTipoActividad.java
+++ Entrega2/src/UI/PTipoActividad.java
@@ -1,7 +1,10 @@
 package UI;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 
 public class PTipoActividad extends JPanel{
-
+	
+	
 }
